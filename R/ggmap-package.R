d866355dffae519ba7c2048a85d6417b2be7b7d6
Michael Chirico <chiricom@google.com>
2025-05-29 01:29:30 -0700

    also in ggmap-package.R

--- R/ggmap-package.R
+++ R/ggmap-package.R
@@ -2,7 +2,7 @@
 #' @name ggmap
 #' @importFrom png readPNG
 #' @importFrom jpeg readJPEG
-#' @importFrom plyr arrange compact ddply desc dlply is.discrete is.formula join
+#' @importFrom plyr arrange compact ddply desc is.discrete is.formula join
 #'   ldply .
 #' @importFrom bitops bitOr bitShiftL bitShiftR bitAnd
 #' @importFrom grDevices as.raster extendrange gray rgb
