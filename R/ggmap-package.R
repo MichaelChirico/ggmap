b98501cb8fa92382fe7a890edd72a92fa430e757
Michael Chirico <michaelchirico4@gmail.com>
2025-06-02 16:28:26 -0700

    Drop unused plyr::join() import

--- R/ggmap-package.R
+++ R/ggmap-package.R
@@ -3,7 +3,7 @@
 #' @name ggmap
 #' @importFrom png readPNG
 #' @importFrom jpeg readJPEG
-#' @importFrom plyr arrange compact ddply desc dlply is.discrete is.formula join
+#' @importFrom plyr arrange compact ddply desc dlply is.discrete is.formula
 #'   ldply llply .
 #' @importFrom bitops bitOr bitShiftL bitShiftR bitAnd
 #' @importFrom grDevices as.raster extendrange gray rgb
