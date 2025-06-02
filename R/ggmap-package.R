071abde44a879e404781381ad3ae2e230aa12f31
Michael Chirico <michaelchirico4@gmail.com>
2025-06-02 16:30:10 -0700

    join() also unused

--- R/ggmap-package.R
+++ R/ggmap-package.R
@@ -2,7 +2,7 @@
 #' @name ggmap
 #' @importFrom png readPNG
 #' @importFrom jpeg readJPEG
-#' @importFrom plyr arrange compact ddply desc is.discrete is.formula join
+#' @importFrom plyr arrange compact ddply desc is.discrete is.formula
 #'   ldply .
 #' @importFrom bitops bitOr bitShiftL bitShiftR bitAnd
 #' @importFrom grDevices as.raster extendrange gray rgb
